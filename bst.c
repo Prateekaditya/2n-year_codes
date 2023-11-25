a6e38c2bb18b87c94275ed3247d026551eabac4c
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2023-11-25 23:56:10 +0530

    Add files via upload

--- bst.c
+++ bst.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct n{
+typedef struct node{
   int info;
-  struct n *lchild,*rchild;
+  struct node *lchild,*rchild;
 }node;
+node *root=NULL;
 node * makenode(){
     node *nw;
     nw=(node *)malloc(size(node));
@@ -11,8 +12,34 @@ node * makenode(){
     nw->rchild=NULL;
     return (nw);
 }
-
+int insert(int data){
+    node *p,*nw,*q;
+    nw=makenode();
+    nw->info=data;
+    p=root;
+    while(p!=NULL){
+        {q=p;
+        if (p->info>data)
+        {
+            p=p->lchild;
+        }
+        else{
+            p=p->rchild;
+        }}
+        if(q->info>data){
+            q->lchild=nw;
+        }
+        else{q->rchild=nw;
+        }
+    }
+}
 int main()
 {
-    node *root=NULL;
+    int data;
+    printf("enter data;");
+    scanf("%d",&data);
+    insert(data)
+  
+    return 0;
+
 }
