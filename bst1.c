db03db0c788f3462759f0671fe88cc654580ef09
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2023-11-25 23:55:19 +0530

    Add files via upload

--- bst1.c
+++ bst1.c
@@ -1,55 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node {
-int data;
-struct node *left, *right;
+struct node{
+    int data;
+    struct node *left,*right;
 };
-
-struct node *newNode(int data)
- { struct node *temp = (struct node *)malloc(sizeof(struct node));
-  temp->data = data;
-  temp->left = NULL;
-  temp->right = NULL;
-  return temp;
+struct node * newnode(int data){
+    struct node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->left=temp->right=NULL;
+    return temp;
 }
+struct node *insert(struct node *root,int data){
+    if(root==NULL){
+        return newnode(data);
+    }
+    if(data<root->data){
+        root->left=insert(root->left,data);
 
-struct node *insert(struct node *root, int data)
-{
-  if (root == NULL) {
-    return newNode(data);
-  }
-  if (data < root->data) {
-    root->left = insert(root->left, data);
-  } else if (data > root->data) {
-    root->right = insert(root->right, data);
-  } else {
-    printf("Duplicate data %d already exists in the BST\n", data);
-  }
-
-  return root;
+    }
+    else if(data>root->data){
+        root->right=insert(root->right,data);
+    }
+    else{
+        printf("duplicate data %d already exist in the BST\n");
+    }
+    return root;
 }
-void inOrderTraversal(struct node *root)
-{
-  if (root == NULL) {
-    return;
-  }
-  inOrderTraversal(root->left);
-  printf("%d ", root->data);
-  inOrderTraversal(root->right);
+void inorder(struct node * root){
+    if(root==NULL){
+        return ;
+    }
+    inorder(root->left);
+    printf("%d\t",root->data);
+    inorder(root->right);
 }
+int main(){
+    struct node *root=NULL;
+    int data,n,i;
+    printf("Enter the number of nodes:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("Enter the data for BST:");
+        scanf("%d",&data);
+        root=insert(root,data);
+    }
+    printf("INORDER TRAVERSAL:");
+    inorder(root);
 
-int main() 
-{
-  struct node *root =NULL;
-  int data,n,i;
-  printf("Enter the numbers of nodes:");
-  scanf("%d",&n);
-  for(i=0;i<n;i++){
-    printf("Enter data:");
-    scanf("%d",&data);
-  root = insert(root, data);}
-  printf("In-order traversal: ");
-  inOrderTraversal(root);
 
-  return 0;
+    return 0;
 }
