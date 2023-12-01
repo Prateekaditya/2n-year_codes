75d1f50e7452f1ad2adef11d7287e82351c44176
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2023-12-02 00:36:48 +0530

    Add files via upload

--- bubble1.c
+++ bubble1.c
@@ -1,23 +1,40 @@
-//BUBBLE SORT
-#include<stdio.h>
-void bubbleSort(int arr[], int n){
-  int i, j;
+
+#include <stdio.h>
+
+int main() {
+  int n = 100; // Define the array size explicitly
+  int a[n]; // Declare the array with the specified size
+  int i, j, flag, temp;
+
+  printf("Enter the size of the array:");
+  scanf("%d", &n); // Read the array size (but unused since n is already defined)
+
+  printf("Enter the array elements:");
+  for (i = 0; i < n; i++) {
+    scanf("%d", &a[i]); // Read input into the array
+  }
+
   for (i = 0; i < n - 1; i++) {
-    for (j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+    flag = 0;
+
+    for (j = 0; j < n - 1 - i; j++) {
+      if (a[j] > a[j + 1]) {
+        temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+        flag = 1;
       }
     }
+
+    if (flag == 0) {
+      break;
+    }
   }
-}
-int main(){
-  int arr[] = {64, 34, 25, 12, 22, 11, 90};
-  int n = sizeof(arr) / sizeof(arr[0]);
-  bubbleSort(arr, n);
-  printf("Sorted array: \n");
-  for (int i = 0; i < n; i++)
-    printf("%d ", arr[i]);
+
+  printf("Sorted array: ");
+  for (i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+
   return 0;
 }
