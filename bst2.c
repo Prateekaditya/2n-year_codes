527ca929fba15e9fbe5310a0322b5ee93db21c03
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2024-02-17 19:25:25 +0530

    Add files via upload

--- bst2.c
+++ bst2.c
@@ -39,41 +39,47 @@ struct node *findMin(struct node *root)
 
   return root;
 }
-struct node *deleteNode(struct node *root, int data)
-{
-  if (root == NULL) {
+struct node *findmin(struct node *root){
+  if(root==NULL){
     return NULL;
   }
-  if (data < root->data) {
-    root->left = deleteNode(root->left, data);
-  } else if (data > root->data) {
-    root->right = deleteNode(root->right, data);
-  } else {
-    // Node to be deleted found
-    // Check if the node has no children
-    if (root->left == NULL && root->right == NULL) {
+  while(root->left!=NULL){
+    root=root->left;
+  }
+  return root;
+}
+struct node * delete(struct node *root,int data){
+  if(root==NULL){
+    return NULL;
+  }
+  if(data<root->data){
+    root->left=delete(root->left,data);
+  }
+  else if(data>root->data){
+    root->right=delete(root->right,data);
+  }
+  else{
+    //no child
+    if(root->left==NULL && root->right==NULL){
       free(root);
       return NULL;
     }
-    // Check if the node has one child
-if (root->left == NULL) {
-      struct node *temp = root->right;
-      free(root);
-      return temp;
-    } else
- 
-if (root->right == NULL) {
-      struct node *temp = root->left;
+    //one child
+    if(root->left==NULL){
+      struct node *temp=root->right;
       free(root);
       return temp;
     }
-
-    // Check if the node has two children
-    struct node *successor = findMin(root->right);
-    root->data = successor->data;
-    root->right = deleteNode(root->right, successor->data);
+    if(root->right==NULL){
+      struct node *temp=root->left;
+      free(root);
+      return temp;
+    }
+    //two child
+    struct node *successor=findmin(root->right);
+    root->data=successor->data;
+    root->right=delete(root->right,successor->data);
   }
-
   return root;
 }
 
@@ -104,12 +110,12 @@ int main()
   inOrderTraversal(root);
   // Deleting nodes from the BST
    int j,m;
-   printf("Enter the no.of nodes to be deleted:");
+   printf("\nEnter the no.of nodes to be deleted:");
    scanf("%d",&m);
    for(j=0;j<m;j++){
     printf("Enter the data to be deleted:");
     scanf("%d",&data);
-    root=deleteNode(root,data);
+    root=delete(root,data);
    }
   // In-order traversal of the modified BST
   printf("In-order traversal: ");
